18b46ef7d7dcb569eb434e7c10cb7023c001c3de
Jocsan-hernandez93 <jhernandezb4@estudiante.uc.cl>
2026-03-25 09:22:37 -0600

    Implementacion del modelo de movimiento

--- depositos/models.py
+++ depositos/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from mongoengine import Document, ReferenceField, FloatField, StringField, DateField, DateTimeField
+from datetime import datetime, timezone
+from usuarios.models import User
 
-# Create your models here.
+class Movimiento(Document):
+    user = ReferenceField(User, required=True)
+    monto = FloatField(required=True)
+    tipo = StringField(
+        choices=["capitalInicial", "interes", "retiro"],
+        required=True
+    )
+    descripcion = StringField()
+
+    fecha_operacion = DateField(required=True) 
+    created_at = DateTimeField(default=lambda: datetime.now(timezone.utc)) 
+
+    meta = {
+        "collection": "movimientos",
+        "ordering": ["-fecha_operacion", "-created_at"],
+        "indexes": [
+            "user",
+            "tipo",
+            "fecha_operacion"
+        ]
+    }
